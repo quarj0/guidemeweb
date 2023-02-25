3eeed5c1e25b34dc2076ec0c460d2a32c3b6835d
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-02-25 11:08:38 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-import './custom.css'
+import './index.css';
 
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-import Home from './components/Home';
+
 import Tutorial1 from './components/Tutorial1';
 import Tutorial2 from './components/Tutorial2';
 import Tutorial3 from './components/Tutorial3';
-
+import Python from './components/images/Python.png';
+import DataScince from './components/images/analysis.png';
+import Script from './components/images/script.jpeg';
 function App() {
   return (
     <Router>
@@ -30,6 +32,32 @@ function App() {
         </footer>
       </div>
     </Router>
+  );
+}
+
+function Home() {
+  return (
+    <section>
+      <h2>Our Tutorials</h2>
+      <div className="tutorial">
+        <img src={Python} alt="Tutorial 1" />
+        <h3>Tutorial 1</h3>
+        <p>Python the most popular programming language among...</p>
+        <Link to="/tutorial1"><button>Read More</button></Link>
+      </div>
+      <div className="tutorial">
+        <img src={DataScince} alt="Tutorial 2" />
+        <h3>Analysis With Python</h3>
+        <p>Data Analysis is the most...</p>
+        <Link to="/tutorial2"><button>Read More</button></Link>
+      </div>
+      <div className="tutorial">
+        <img src={Script} alt="Tutorial 3" />
+        <h3>Tutorial 3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Link to="/tutorial3"><button>Read More</button></Link>
+      </div>
+    </section>
   );
 }
 
--- src/components/Tutorial1.js
+++ src/components/Tutorial1.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Tutorial1() {
   return (
     <div className="tutorial-page">
-      <h2>Tutorial 1</h2>
-      <p>This is a tutorial on Python.</p>
+      <header className="tutorial-header">
+        <h1>Kali Linux Tutorial</h1>
+      </header>
+      <main className="tutorial-content">
+        <p>
+          In this tutorial, we will be learning how to use Kali Linux to perform
+          vulnerability assessments on a target website. We will be using a
+          variety of tools and techniques to identify and exploit weaknesses in
+          the website's security.
+        </p>
+        <p>
+          Before we begin, you will need to have Kali Linux installed on your
+          machine. If you haven't already done so, you can download Kali Linux
+          from the official website:
+        </p>
+        <a href="https://www.kali.org/downloads/">https://www.kali.org/downloads/</a>
+        <p>
+          Once you have Kali Linux installed, we can begin the tutorial by
+          launching the Metasploit Framework and scanning our target website
+          for vulnerabilities.
+        </p>
+      </main>
+      <footer className="tutorial-footer">
+        <Link to="/">Back to Tutorials</Link>
+      </footer>
     </div>
   );
 }
